9f5e996cc513a4990c8ea6b3ffba64768a54307a
osaeman <2015n2424@gmail.com>
2023-11-14 00:08:41 +0500

    all dropdowns

--- js/app.js
+++ js/app.js
@@ -10,20 +10,28 @@ hamburger_btn.addEventListener("click", () => {
 
 slider_close_btn.addEventListener("click", () => {
   mobile_nav_slider.classList.remove("mobile_nav_slide_show");
-  mobile_nav_dropdown.classList.remove("mobile-nav-dropdown-show");
-  mobile_nav_link_i.innerHTML = `<i class="far fa-plus"></i>`;
+  mobile_nav_dropdown.forEach((item, index) => {
+    mobile_nav_dropdown[index].classList.remove("mobile-nav-dropdown-show");
+    mobile_nav_link_i[index].innerHTML = `<i class="far fa-plus"></i>`;
+    mobile_nav_link[index].childNodes[1].style.color = "white";
+  });
 });
 
-const mobile_nav_link = document.querySelector(".mobile-nav-link");
-const mobile_nav_link_i = document.querySelector(".mobile-nav-link-i");
-const mobile_nav_dropdown = document.querySelector(".mobile-nav-dropdown");
-mobile_nav_link.addEventListener("click", () => {
-  mobile_nav_dropdown.classList.toggle("mobile-nav-dropdown-show");
-  if (mobile_nav_dropdown.classList.contains("mobile-nav-dropdown-show")) {
-    mobile_nav_link.childNodes[1].style.color = "#006c39";
-    mobile_nav_link_i.innerHTML = `<i class="far fa-minus"></i>`;
-  } else {
-    mobile_nav_link_i.innerHTML = `<i class="far fa-plus"></i>`;
-    mobile_nav_link.childNodes[1].style.color = "white";
-  }
+const mobile_nav_link = document.querySelectorAll(".mobile-nav-link");
+const mobile_nav_link_i = document.querySelectorAll(".mobile-nav-link-i");
+const mobile_nav_dropdown = document.querySelectorAll(".mobile-nav-dropdown");
+
+mobile_nav_link.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    mobile_nav_dropdown[index].classList.toggle("mobile-nav-dropdown-show");
+    if (
+      mobile_nav_dropdown[index].classList.contains("mobile-nav-dropdown-show")
+    ) {
+      mobile_nav_link[index].childNodes[1].style.color = "#006c39";
+      mobile_nav_link_i[index].innerHTML = `<i class="far fa-minus"></i>`;
+    } else {
+      mobile_nav_link_i[index].innerHTML = `<i class="far fa-plus"></i>`;
+      mobile_nav_link[index].childNodes[1].style.color = "white";
+    }
+  });
 });
